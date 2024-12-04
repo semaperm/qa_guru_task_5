b22e24a5e1ace74e1f0bb43572b4d5097682ff08
Sema Butsyk <semakontakt@mail.ru>
2024-12-04 15:53:58 +0500

    fix ver.2

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,10 +4,8 @@ def test_dark_theme_by_time():
 
     current_time = time(hour=23)
 
-    if time(hour=22) <= current_time or current_time < time(hour=6):
-        return True
-    else:
-        return False
+    if time(hour=22) <= current_time or current_time <= time(hour=6):
+        is_dark_them = True
 
     assert is_dark_theme is True
 
@@ -17,15 +15,13 @@ def test_dark_theme_by_time_and_user_choice():
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
 
-    if dark_theme_enabled_by_user:
-        if time(hour=22) <= current_time or current_time < time(hour=6):
-            return True
+    if dark_theme_enabled_by_user == None:
+        if time(hour=22) <= current_time or current_time <= time(hour=6):
+            is_dark_them = True
         else:
-            return False
-    elif dark_theme_enabled_by_user:
-        return True
+            is_dark_them = False
     else:
-        return False
+        is_dark_theme = dark_theme_enabled_by_user
 
     assert is_dark_theme is True
 
@@ -43,15 +39,13 @@ def test_find_suitable_user():
     ]
     for user in users:
         if user['name'] == 'Olga':
-            return user
-
-    suitable_users = user
+            suitable_users = user
 
     assert suitable_users == {"name": "Olga", "age": 45}
 
     suitable_users = []
     for user in users:
-        if users ['age'] < 20:
+        if user['age'] < 20:
             suitable_users.append(user)
 
     assert suitable_users == [
@@ -78,18 +72,18 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = open_browser
+    actual_result = print_function_and_args(open_browser, browser_name)
 
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = go_to_companyname_homepage
+    actual_result = print_function_and_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = find_registration_button_on_login_page
+    actual_result = print_function_and_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
 
 def print_function_and_args(func, *args):
