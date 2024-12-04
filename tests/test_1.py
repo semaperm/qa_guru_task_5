dc257cffb4bf728fa528acb2c80bb90e00fe2fb9
semaperm <semakontakt@gmail.com>
2024-12-05 02:00:09 +0500

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -19,7 +19,7 @@ def test_dark_theme_by_time_and_user_choice():
         if time(hour=22) <= current_time or current_time <= time(hour=6):
             is_dark_theme = True
         else:
-            is_dark_them = False
+            is_dark_theme = False
     else:
         is_dark_theme = dark_theme_enabled_by_user
 
